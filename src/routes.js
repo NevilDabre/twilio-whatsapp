775ea962d5fb09c64f236a546f8bca5441dc911b
NEVILLE DABRE <neville.thamor@gmail.com>
2021-02-26 17:44:58 -0500

    feat: homepage

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { processAndSendReply} = require('./send-message');
 
+router.get('/',(req,res)=> res.send('Twilio-Whatsapp automated messaging.'))
 router.post('/rates', processAndSendReply);
 
 module.exports = { router };
